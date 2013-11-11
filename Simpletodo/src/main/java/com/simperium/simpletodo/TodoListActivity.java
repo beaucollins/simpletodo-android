82246868603b517b723dab2865bb55ff972e0b93
Beau Collins <beaucollins@gmail.com>
2013-11-11 14:24:15 -0800

    Starting bare app

---
+++ Simpletodo/src/main/java/com/simperium/simpletodo/TodoListActivity.java
@@ -0,0 +1,14 @@
+package com.simperium.simpletodo;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+
+public class TodoListActivity extends ListActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
